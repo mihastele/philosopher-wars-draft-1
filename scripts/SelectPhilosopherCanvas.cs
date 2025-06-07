a4a5be18f7e1ff78353e8afebb1fae196e9eb8c9
Miha <stele1000@gmail.com>
2025-06-07 08:40:40 +0200

    WIP fail

--- scripts/SelectPhilosopherCanvas.cs
+++ scripts/SelectPhilosopherCanvas.cs
@@ -15,20 +15,44 @@ public partial class SelectPhilosopherCanvas : CanvasLayer
 
 	// Dictionary storing philosopher images
 	private Dictionary<string, Texture2D> philosopherTextures = new Dictionary<string, Texture2D>();
+	// private Dictionary<string, Sprite2D> philosopherSprites = new Dictionary<string, Sprite2D>();
+	
+	[Export]
+	private Godot.Collections.Array<TextureRect> philosopherRects;
+	
 
 	public override void _Ready()
 	{
 		// Load philosopher images
-		philosopherTextures["Nietzsche"] = GD.Load<Texture2D>("res://images/nietzsche.png");
-		philosopherTextures["Descartes"] = GD.Load<Texture2D>("res://images/descartes.png");
-		philosopherTextures["Kant"] = GD.Load<Texture2D>("res://images/kant.png");
-		philosopherTextures["Socrates"] = GD.Load<Texture2D>("res://images/socrates.png");
+		philosopherTextures["Nietzsche"] = GD.Load<Texture2D>("res://philosophers/Nietzsche.png");
+		philosopherTextures["Descartes"] = GD.Load<Texture2D>("res://philosophers/Descartes.png");
+		philosopherTextures["Kant"] = GD.Load<Texture2D>("res://philosophers/Kant.png");
+		philosopherTextures["Socrates"] = GD.Load<Texture2D>("res://philosophers/Socrates.png");
+		// philosopherSprites["Nietzsche"] = new Sprite2D();
+		// philosopherSprites["Descartes"] = new Sprite2D();
+		// philosopherSprites["Kant"] = new Sprite2D();
+		// philosopherSprites["Socrates"] = new Sprite2D();
+		// philosopherSprites["Nietzsche"].Texture = philosopherTextures["Nietzsche"];
+		// philosopherSprites["Descartes"].Texture = philosopherTextures["Descartes"];
+		// philosopherSprites["Kant"].Texture = philosopherTextures["Kant"];
+		// philosopherSprites["Socrates"].Texture = philosopherTextures["Socrates"];
 
 		// Assign textures to buttons
 		NietzscheButton.TextureNormal = philosopherTextures["Nietzsche"];
 		DescartesButton.TextureNormal = philosopherTextures["Descartes"];
 		KantButton.TextureNormal = philosopherTextures["Kant"];
 		SocratesButton.TextureNormal = philosopherTextures["Socrates"];
+
+		// Scale the buttons dynamically
+		NietzscheButton.RectScale = new Vector2(0.15, 0.15);
+		DescartesButton.RectScale = new Vector2(0.15, 0.15);
+		KantButton.RectScale = new Vector2(0.15, 0.15);
+		SocratesButton.RectScale = new Vector2(0.15, 0.15);
+		// // Scale the TextureRect
+		// NietzscheButton.TextureNormal.scale = new Vector2(0.15, 0.15);
+		// DescartesButton.TextureNormal.scale = new Vector2(0.15, 0.15);
+		// KantButton.TextureNormal.scale = new Vector2(0.15, 0.15);
+		// SocratesButton.TextureNormal.scale = new Vector2(0.15, 0.15);
 
 		// Connect button signals
 		NietzscheButton.Pressed += OnNietzschePressed;
